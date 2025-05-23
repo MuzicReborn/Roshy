f781afc8e3f242fb22236ffd8daa9cc64b3dace0
MuzicReborn <muzicreborn@gmail.com>
2025-05-24 03:21:00 +0530

    Update Script

--- script.py
+++ script.py
@@ -7,18 +7,20 @@ import subprocess
 count = 1
 ts_links = []
 qualities = []
+downloads_folder = os.path.expanduser('~') + "/Downloads/"
 
 def download_file(file_url, file_name):
+    file_name = downloads_folder + file_name
     req = urllib.request.Request(file_url, headers={"Referer": "https://turtleviplay.xyz/"})
     with urllib.request.urlopen(req) as response:
         with open(file_name, "wb") as f:
             f.write(response.read())
     if len(ts_links) != 0:
-        print(f"Downloaded {file_name[:file_name.find('.')]}/{len(ts_links)}")
+        print(f"Downloaded {file_name[file_name.rfind('/') + 1 : file_name.find('.')]}\\{len(ts_links)}")
     else:
         print(f"Downloaded {file_name}!")
 
-os.makedirs("ts", exist_ok=True)
+os.makedirs(downloads_folder + "ts", exist_ok=True)
 
 url = input("Enter url: ")
 name = input("Enter file name: ")
@@ -28,7 +30,7 @@ main_url = url[:url.rfind("m3u8")][:url[:url.rfind("m3u8")].rfind("/") + 1]
 
 # Downloading index
 download_file(url, "index.m3u8")
-with open("index.m3u8") as index:
+with open(f"{downloads_folder}index.m3u8") as index:
     for line in index.readlines():
         if "RESOLUTION" in line:
             quality = line[line.find("RESOLUTION=") + 11:line.find("RESOLUTION=") + 11 + line[line.find("RESOLUTION=") + 11:].find(",")]
@@ -45,23 +47,23 @@ main_url = url[:url.rfind("m3u8")][:url[:url.rfind("m3u8")].rfind("/") + 1]
 download_file(qualities[choice], "index.m3u8")
 qualities.clear()
 
-with open("index.m3u8") as playlist:
+with open(f"{downloads_folder}index.m3u8") as playlist:
     for line in playlist.readlines():
         if "EXT" not in line and "https" not in line:
             ts_links.append(f"{main_url}{line.strip()}")
         elif "https" in line:
             ts_links.append(f"{line.strip()}")
 
-fileNames = [f"ts/{i}.ts" for i in range(len(ts_links))]
+fileNames = [f"ts/{i}.ts" for i in range(1, len(ts_links) + 1)]
 
 print(f"Downloading {len(ts_links)} files...")
 with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
     executor.map(download_file, ts_links, fileNames)
 
 # Append downloaded ts to single file
-with open("out.ts", 'wb') as output:
+with open(downloads_folder + "out.ts", 'wb') as output:
     for i in range(1, len(ts_links) + 1):
-        file = f"ts/{i}.ts"
+        file =  downloads_folder + f"ts/{i}.ts"
         if os.path.exists(file):
             with open(file, 'rb') as ts_file:
                 content = ts_file.read()
@@ -70,14 +72,14 @@ with open("out.ts", 'wb') as output:
 # ffmpeg to convert to mp4
 ffmpeg_command = [
     "ffmpeg",
-    "-i", "out.ts",
+    "-i", downloads_folder + "out.ts",
     "-c:v", "copy",
     "-c:a", "copy",
-    f"{name}.mp4"
+    downloads_folder + f"{name}.mp4"
 ]
 subprocess.run(ffmpeg_command)
 
 # Clean up
-shutil.rmtree("ts")
-os.remove("out.ts")
-os.remove("index.m3u8")
+shutil.rmtree(downloads_folder + "ts")
+os.remove(downloads_folder + "out.ts")
+os.remove(downloads_folder + "index.m3u8")
